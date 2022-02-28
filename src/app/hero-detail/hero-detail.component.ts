d32ff4751669ef4a071c0572797f26c7a9face19
Alassane NIANG <adniang75@gmail.com>
2022-02-28 19:02:18 +0100

    Get Data From a Server

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,22 +1,22 @@
-import { Component, OnInit } from "@angular/core";
-import { Hero } from "../hero";
-import { ActivatedRoute } from "@angular/router";
-import { HeroService } from "../hero.service";
-import { Location } from "@angular/common";
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component ( {
-    selector: "app-hero-detail",
-    templateUrl: "./hero-detail.component.html",
-    styleUrls: [ "./hero-detail.component.css" ],
+    selector: 'app-hero-detail',
+    templateUrl: './hero-detail.component.html',
+    styleUrls: [ './hero-detail.component.css' ]
 } )
 export class HeroDetailComponent implements OnInit {
-
     hero : Hero | undefined;
 
     constructor (
         private route : ActivatedRoute,
         private heroService : HeroService,
-        private location : Location,
+        private location : Location
     ) {
     }
 
@@ -25,7 +25,7 @@ export class HeroDetailComponent implements OnInit {
     }
 
     getHero () : void {
-        const id = Number ( this.route.snapshot.paramMap.get ( "id" ) );
+        const id = parseInt ( this.route.snapshot.paramMap.get ( 'id' )!, 10 );
         this.heroService.getHero ( id )
             .subscribe ( hero => this.hero = hero );
     }
@@ -34,4 +34,10 @@ export class HeroDetailComponent implements OnInit {
         this.location.back ();
     }
 
+    save () : void {
+        if ( this.hero ) {
+            this.heroService.updateHero ( this.hero )
+                .subscribe ( () => this.goBack () );
+        }
+    }
 }
